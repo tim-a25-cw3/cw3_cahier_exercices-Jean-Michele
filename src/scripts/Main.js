42c010245d0aa620942b4614ca4836ad104e11a2
Jean-Michele <morin.emilemorin@gmail.com>
2025-11-17 12:33:10 -0500

    a
    a

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,12 +1,20 @@
 import Icons from './utils/Icons.js';
-
+import Swiper from 'swiper/bundle';
 class Main {
   constructor() {
     this.init();
+    this.initSwiperBase();
   }
 
   init() {
     Icons.load();
+    const swiper = new Swiper('.js-swiper', {
+      speed: 400,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
   }
 }
 new Main();
